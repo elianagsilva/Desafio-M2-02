34dd1ce1e26e6ba57796095c101e2a3c569ca94c
elianagsilva <elianacrisa60@gmail.com>
2022-07-08 19:59:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let idade = 18
+
+if(idade>18){
+    console.log("Você é maior de idade!")
+}else{
+    console.log("Você é menor de idade!")
+}
+
+
+let human = false
+
+if(human == true && idade >= 18){
+    console.log("Você é humano")
+}else{
+    console.log("Você não é humano!")
+}
+
+
+let aniversario = "março"
+
+if(aniversario == "janeiro" || aniversario || "dezembro"){
+    console.log("Parabéns!")
+}else{
+    console.log("Hoje não é seu aniversário.")
+}
+
+const nome = "Eliana"
+
+if(nome[0] === "R"){
+    console.log("Seu nome começa com a letra R")
+}else{
+    console.log("Seu nome não começa com a letra R")
+}
+
+let sobrenome = "Silva"
+
+if(sobrenome.lenght >= 6 || nome[0] == "E"){
+    console.log("Nome bonito!")
+}else{
+    console.log("Nome maneiro!")
+}
